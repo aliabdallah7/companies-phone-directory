7401297cf7c237e730a3425d5838512e99e20f63
Ali Abdallah <ali.abdallah43792@gmail.com>
2023-02-16 02:08:01 +0200

    Initial commit

---
+++ assembly/index.ts
@@ -0,0 +1,30 @@
+import { phoneDirectory, Company } from "./models";
+
+export function registerCompany(
+  creatorId: string,
+  name: string,
+  phone: string
+): string {
+  const company = new Company(creatorId, name, phone);
+  phoneDirectory.push(company);
+  return "Company " + name + " has been registered! by " + creatorId;
+}
+
+export function getCompaniesData(): Array<Company> {
+  let companies = new Array<Company>(phoneDirectory.length);
+  for (let i = 0; i < phoneDirectory.length; i++) {
+    let company = phoneDirectory[i];
+    companies[i] = company;
+  }
+  return companies;
+}
+
+export function getCompany(name: string): Company {
+  for (let i = 0; i < phoneDirectory.length; i++) {
+    let company = phoneDirectory[i];
+    if (company.name == name) {
+      return company;
+    }
+  }
+  return new Company("", "", "");
+}
